d1a912874bf83a1390205435130025ee08b7fa3f
DongWoon <118097194+dasima1125@users.noreply.github.com>
2024-11-22 22:46:02 +0900

    [level 2] Title: 주식가격, Time: 3.57 ms, Memory: 38.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/42584. 주식가격/주식가격.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+
+
+public class Solution {
+    public int[] solution(int[] prices) {
+        
+        List<int> list = new List<int>();
+        for (int i = 0; i < prices.Length; i++)
+        {
+            int count = 0;
+            for (int j = i + 1 ; j < prices.Length; j++) 
+            {
+                count++;
+                
+                if (prices[j] < prices[i])
+                {
+                    break;
+                }
+            }
+            list.Add(count);
+        }
+        return list.ToArray();
+    }
+}
