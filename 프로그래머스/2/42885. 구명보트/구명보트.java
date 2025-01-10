62827a22f281cfff73f6cf5caf0b36481e725e20
DongWoon <118097194+dasima1125@users.noreply.github.com>
2025-01-10 23:18:27 +0900

    [level 2] Title: 구명보트, Time: 10.66 ms, Memory: 55.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int[] people, int limit) {
+        Arrays.sort(people);
+        int light = 0;
+        int heavy = people.length - 1;
+        int count = 0;
+        while (light <= heavy) 
+        {  
+            if (people[light] + people[heavy] <= limit) 
+            {
+                light++; 
+            }
+            heavy--; 
+            count++; 
+        }
+        return count;
+    }
+}
