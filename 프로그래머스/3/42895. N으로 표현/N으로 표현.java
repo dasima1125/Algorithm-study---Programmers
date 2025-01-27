c612161544ee7df4cdbd7e276e78c717718d182c
DongWoon <118097194+dasima1125@users.noreply.github.com>
2025-01-27 22:50:34 +0900

    [level 3] Title: N으로 표현, Time: 26.70 ms, Memory: 86.8 MB -BaekjoonHub

--- 프로그래머스/3/42895. N으로 표현/N으로 표현.java
+++ 프로그래머스/3/42895. N으로 표현/N으로 표현.java
@@ -1,34 +1,36 @@
 import java.util.*;
 class Solution {
     public static int solution(int N, int number) {
-        Set<Integer>[] set = new Set[9];
-        for(int i = 1; i < 9; i++) 
+        Set<Integer>[] dp = new Set[9]; 
+        for (int i = 0; i < 9; i++) 
         {
-            set[i] = new HashSet<Integer>();
-        }    
-        for(int i = 1; i < 9; i++) 
-        {
-            set[i].add(Integer.parseInt(String.valueOf(N).repeat(i)));
-            for(int j = 1;j < i; j++ ) 
+            dp[i] = new HashSet<>();
+        }
+    
+        for (int i = 1; i < 9; i++) 
+        {  
+            dp[i].add(Integer.parseInt(String.valueOf(N).repeat(i)));
+            for (int j = 1; j < i; j++) 
             {
-                for(Integer x : set[j]) 
+                for (int num1 : dp[j]) 
                 {
-                    for(Integer y : set[i-j]) 
+                    for (int num2 : dp[i - j]) 
                     {
-                        set[i].add(x+y);
-                        set[i].add(x-y);
-                        set[i].add(y-x);
-                        set[i].add(x*y);
-                        if(x != 0) set[i].add(y/x);
-                        if(y != 0) set[i].add(x/y);
+                        dp[i].add(num1 + num2); // 더하기
+                        dp[i].add(num1 - num2); // 빼기
+                        dp[i].add(num1 * num2); // 곱하기
+                        if (num2 != 0) dp[i].add(num1 / num2);//나누기 소수점은 알아서 쳐내고
+                        
                     }
                 }
+                
             }
-            if(set[i].contains(number)) {
+
+            if (dp[i].contains(number)) 
+            {
                 return i;
             }
         }
-
         return -1;
     }
     
