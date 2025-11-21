2a025e75b3434846dd5b8e3b706c0c90d418be3b
DongWoon <118097194+dasima1125@users.noreply.github.com>
2025-11-22 02:30:23 +0900

    [level 2] Title: 기능개발, Time: 3.84 ms, Memory: 80.4 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -3,30 +3,24 @@ import java.util.*;
 class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
         
-        List<Integer> completeday = new ArrayList<>();
         List<Integer> result = new ArrayList<>();
         
-        for(int i = 0; i < progresses.length; i++)
-        {
-             completeday.add((int)Math.ceil((100.0 - progresses[i]) / speeds[i]));// 반올림 필수 그냥 인트걸면 잔여일 날라감
-        }
-        int MaxDay = completeday.get(0);
+        int currentMaxDay = (int)Math.ceil((100.0 - progresses[0]) / speeds[0]);
         int count = 1;
-         
-        for(int i = 1; i < completeday.size();i++)
+        
+        for(int i = 1; i < progresses.length; i++) 
         {
-            // 기준일 이하
-            if(completeday.get(i) <= MaxDay)
-                count++;
-            // 기준일 초과
-            else
+            int day = (int)Math.ceil((100.0 - progresses[i]) / speeds[i]);
+            
+            if(day <= currentMaxDay) count++;
+            else 
             {
-                result.add(count);           //그 동안 작업 적재
-                MaxDay = completeday.get(i); //신규 기준일 재배치
-                count = 1;                   //배포수량 초기화
+                result.add(count);
+                currentMaxDay = day;
+                count = 1;
             }
         }
-        //남은배포수량 추가 
+        
         result.add(count);
         return result.stream().mapToInt(i -> i).toArray();
     }
