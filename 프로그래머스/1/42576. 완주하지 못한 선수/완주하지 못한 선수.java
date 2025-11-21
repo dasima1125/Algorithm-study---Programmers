93506adaa2b338c4328d31bc4a5d10c1a6428b53
DongWoon <118097194+dasima1125@users.noreply.github.com>
2025-11-21 15:11:37 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 80.11 ms, Memory: 113 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -1,26 +1,18 @@
 import java.util.HashMap;
+
 class Solution {
     public String solution(String[] participant, String[] completion) {
-        String answer = "";
-        HashMap<String,Integer> set = new HashMap<>();
-        for (String string : participant) 
-        {
-            set.put(string, set.getOrDefault(string, 0) + 1); 
-        }
+        HashMap<String, Integer> map = new HashMap<>();
+        
+        for(String part : participant)
+            map.put(part, map.getOrDefault(part,0)+1);
 
-        for (String string : completion) 
-        {
-            set.put(string, set.get(string)-1);
-        }
-      
-        for (String key : set.keySet()) 
-        {
-            if (set.get(key) == 1) 
-            {
-                answer = key;
-                break; 
-            }
-        }
-        return answer;
+        for(String comp : completion)
+            map.put(comp, map.get(comp) - 1);
+
+        for(String key : map.keySet())
+            if (map.get(key) != 0) return key;
+
+        return "";
     }
 }
